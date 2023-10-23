0c20fc86b3a71dfc90b4f6380f84e82bc9025574
louisdpro <122191019+Louisdpr0@users.noreply.github.com>
2023-10-23 14:28:47 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,14 +1,30 @@
-const fs = require('fs')
-const allowedPrefix = ['.confess']; // Prefix yang diizinkan untuk merespon
+const fs = require('fs');
+const allowedPrefix = ['.zeni']; // Prefix yang diizinkan untuk merespon
 
-global.fotomenfess = fs.readFileSync("./menfess.jpg")
-global.owner = ["62887435047326"]
+global.fotomenfess = fs.readFileSync("./menfess.jpg");
+global.owner = ["62887435047326"];
 
 global.help = {
-Menu(pushname){
-  return `Halo ${pushname}
+  Menu(pushname) {
+    return `Halo ${pushname}
 
-Saya adalah bot WhatsApp confess 👋 ketik .confess untuk memulai chat.
-`
+Saya adalah bot WhatsApp confess 👋 ketik .zeni untuk memulai chat.
+    `;
+  },
+};
+
+// Fungsi untuk menangani pesan yang diterima
+function handleMessage(client, message) {
+  const { from, body } = message;
+  const isAllowedPrefix = allowedPrefix.some(prefix => body.startsWith(prefix));
+
+  if (isAllowedPrefix) {
+    // Lakukan sesuatu dengan pesan yang memiliki prefix yang diizinkan
+    // Contoh: balas pesan, lakukan operasi, dll.
+  }
 }
-}
+
+// Event listener untuk pesan yang masuk
+client.on('message', message => {
+  handleMessage(client, message);
+});
