0ab19634c5d173301c04bf1172facacb0d36cf8b
louisdpro <122191019+Louisdpr0@users.noreply.github.com>
2023-10-23 14:11:10 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,1 +1,21 @@
-const fs = require('fs')
+const fs = require('fs');
+const { Client } = require('whatsapp-web.js');
+
+const client = new Client();
+
+client.on('message', async (message) => {
+  if (message.body.startsWith('.start')) {
+    // Tanggapi hanya pesan yang dimulai dengan prefix '.confess'
+    const response = generateResponse(message.sender.pushname);
+    await message.reply(response);
+  }
+});
+
+function generateResponse(pushname) {
+  return `Halo ${pushname}
+
+Saya adalah bot WhatsApp confess 👋 ketik .confess untuk memulai chat.
+  `;
+}
+
+client.initialize();
